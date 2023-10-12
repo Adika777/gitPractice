0cf3fa8c1b6de9318f66894b35ed9f86676e98b8
Adika777 <147472729+Adika777@users.noreply.github.com>
2023-10-12 09:42:36 +0600

    Update VisaGold.java

--- UI/src/test/java/payment/VisaGold.java
+++ UI/src/test/java/payment/VisaGold.java
@@ -2,6 +2,6 @@ package payment;
 
 public class VisaGold {
     public static void main(String[] args) {
-        System.out.println("Visa gold");
+        System.out.println("Visa gold123");
     }
 }
